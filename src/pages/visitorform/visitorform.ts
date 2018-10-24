901e24d8fe33b00a4cf2e3faecf1aa8b73f862fc
Nikhil Philip <nikhilphilip15@gmail.com>
2018-10-24 18:49:18 +0530

    commit

--- src/pages/visitorform/visitorform.ts
+++ src/pages/visitorform/visitorform.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Alert } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import firebase from 'firebase';
 import { FileChooser } from '@ionic-native/file-chooser';
 import { File } from '@ionic-native/file';
