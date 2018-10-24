0442b26bac2e358a7358a592a6516ca7763bc258
Nikhil Philip <nikhilphilip15@gmail.com>
2018-10-24 14:55:53 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { VisitorformPage } from '../pages/visitorform/visitorform';
+
+import { FlatownerregistrationPage } from '../pages/flatownerregistration/flatownerregistration';
  
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = VisitorformPage;
+  rootPage:any = FlatownerregistrationPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
