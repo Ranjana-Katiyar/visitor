ad703377d01ab7ffd4fde8b3df8a7da7d35e0bfc
Ranjana-Katiyar <40160425+Ranjana-Katiyar@users.noreply.github.com>
2018-11-05 18:48:51 +0530

    efwegw

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,25 +2,22 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-<<<<<<< HEAD
+ 
 import { ParceldetailsPage } from '../pages/parceldetails/parceldetails';
 //import { OwnerdashboardPage } from '../pages/ownerdashboard/ownerdashboard';
  
-=======
-import { UploadimagePage } from '../pages/uploadimage/uploadimage';
+ 
 
->>>>>>> 41280c0d2c0a0e9902a81aa909c2e8ce86c433ea
+ 
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
  
-<<<<<<< HEAD
   rootPage:any = ParceldetailsPage;
-=======
-  rootPage:any = UploadimagePage;
->>>>>>> 41280c0d2c0a0e9902a81aa909c2e8ce86c433ea
+ 
+ 
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
