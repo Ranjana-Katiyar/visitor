e9f1375357eb3317a661c47499e71c6d38f13005
Ranjana-Katiyar <40160425+Ranjana-Katiyar@users.noreply.github.com>
2018-11-12 20:02:48 +0530

    dfgdfgfd

--- src/pages/ownerregistration/ownerregistration.ts
+++ src/pages/ownerregistration/ownerregistration.ts
@@ -13,34 +13,74 @@ export class OwnerregistrationPage {
   name:string = "";
   p_number:string = "";
   email:string ="";
- 
+  password:string = "";
+	
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController) {
+    this.ownerRegister();
   }
 
   ownerRegister(){
-    firebase.firestore().collection("owners").add({
-      owner_id : firebase.auth().currentUser.uid,
-      owner_name : firebase.auth().currentUser.displayName,
-      created : firebase.firestore.FieldValue.serverTimestamp(),
-      Name : this.name,
-      phoneNumber : this.p_number,
-      email: this.email
-    }).then((doc) => {
-      console.log(doc);
-       
-      this.toastCtrl.create({
-       message: "Registration Successfull",
-       duration: 1000
-     }).present();
-     this.navCtrl.push(UploadimagePage);
-  }).catch((err)=>{ 
-    console.log(err)
-      this.toastCtrl.create({
-        message: err.message,
-        duration: 3000
-      }).present();
-  })
+    firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+    .then((data)=>{ 
+                    console.log(data) 
+                    let newUser: firebase.User = data.user;
+                    newUser.updateProfile({
+                      displayName: this.name, 
+                      photoURL: ""
+                    })
+                    .then(()=>{
+                                console.log("Profile Updated") 
+                                let user = firebase.auth().currentUser;
+                                user.sendEmailVerification();
+                                
+                                this.toastCtrl.create({
+                                  message: "Verification mail sent",
+                                  duration: 1000
+                                }).present();
+
+                                firebase.firestore().collection("owners").add({
+                                  owner_id : firebase.auth().currentUser.uid,
+                                  owner_name : firebase.auth().currentUser.displayName,
+                                  created : firebase.firestore.FieldValue.serverTimestamp(),
+                                  Name : this.name,
+                                  phoneNumber : this.p_number,
+                                  email: this.email
+                                }).then((doc) => {
+                                  console.log(doc);
+                                   
+                                  this.toastCtrl.create({
+                                   message: "Registration Successfull",
+                                   duration: 1000
+                                 }).present();
+                                 this.navCtrl.push(UploadimagePage);
+                              }).catch((err)=>{ 
+                                console.log(err)
+                                  this.toastCtrl.create({
+                                    message: err.message,
+                                    duration: 3000
+                                  }).present();
+                              })
+
+
+                                
+                    })
+                    .catch((err)=>{ 
+                      console.log(err)
+                        this.toastCtrl.create({
+                          message: err.message,
+                          duration: 3000
+                        }).present();
+                    })
+
+    })
+    .catch((err)=>{ console.log(err)
+                    this.toastCtrl.create({
+                    message: err.message,
+                    duration: 3000
+                  }).present();
+    })
+    
 
  }
 
