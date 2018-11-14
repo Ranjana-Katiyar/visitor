6ec162f50be490bb688e6a0022df14e94ad328c0
Nikhil Philip <nikhilphilip15@gmail.com>
2018-11-14 14:39:57 +0530

    cccc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,7 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-<<<<<<< HEAD
-import {SocietyregistrationPage} from "../pages/societyregistration/societyregistration";
-=======
- 
-
 import {HomePage} from "../pages/home/home";
->>>>>>> 6679716c05a2fcbcd2aae847da8a7b75df41e6d0
-
-
 
 
 @Component({
@@ -18,17 +10,7 @@ import {HomePage} from "../pages/home/home";
 })
 export class MyApp {
 
-
-<<<<<<< HEAD
-  rootPage:any = SocietyregistrationPage;
-=======
- // rootPage:any = HomePage;
   rootPage:any = HomePage;
->>>>>>> 6679716c05a2fcbcd2aae847da8a7b75df41e6d0
-
-
-
-
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
